1fd69a2306ddb4b9c8e8a2af4658c92d1594ffa0
Yuchen Wang <phobosw@gmail.com>
2014-07-17 00:03:20 +0800

    feature:mouse mode, backlight support

--- keyboard/gh60/keymap_monque.c
+++ keyboard/gh60/keymap_monque.c
@@ -5,9 +5,9 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP_ANSI(
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
+        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   FN3, QUOT,     ENT,  \
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
-        LCTL,LGUI,LALT,          SPC,                     RGUI,FN0, FN1, FN2),
+        LCTL,LGUI,LALT,          SPC,                     RALT,FN0, FN1, RCTL),
 
     /* 1: monque custom layer */
     KEYMAP_ANSI(
@@ -22,13 +22,26 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,  \
         TRNS,TRNS,TRNS,UP,  TRNS,TRNS,CALC,TRNS,INS, TRNS,PSCR,SLCK,PAUS,TRNS, \
         CAPS,TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,TRNS,TRNS,HOME,PGUP,     TRNS, \
-        TRNS,TRNS,APP, TRNS,TRNS,TRNS,VOLD,VOLU,MUTE,END, PGDN,          FN3,  \
+        TRNS,TRNS,APP, FN4, FN5, FN6, VOLD,VOLU,MUTE,END, PGDN,          FN2,  \
         TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
+
+    /* 3: Mouse mode (Semicolon/App) */
+    KEYMAP_ANSI(
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,WH_L,WH_D,WH_U,WH_R,TRNS,TRNS,TRNS,TRNS, \
+        TRNS,TRNS,ACL0,ACL1,ACL2,TRNS,MS_L,MS_D,MS_U,MS_R,TRNS,TRNS,     TRNS, \
+        TRNS,TRNS,TRNS,TRNS,TRNS,BTN3,BTN2,BTN1,BTN4,BTN5,TRNS,          TRNS, \
+        TRNS,TRNS,TRNS,          BTN1,                    TRNS,TRNS,TRNS,TRNS),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
-    [1] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
-    [2] = ACTION_LAYER_MOMENTARY(2),  // to Fn overlay
-    [3] = ACTION_LAYER_TOGGLE(1),     // toggle monque custom layer
+    [0] = ACTION_LAYER_MOMENTARY(2),        // to Fn overlay
+    [1] = ACTION_LAYER_MOMENTARY(2),        // to Fn overlay
+
+    [2] = ACTION_LAYER_TOGGLE(1),           // toggle monque custom layer
+    [3] = ACTION_LAYER_TAP_KEY(3, KC_SCLN), // mouse mode
+
+    [4] = ACTION_BACKLIGHT_DECREASE(),
+    [5] = ACTION_BACKLIGHT_TOGGLE(),
+    [6] = ACTION_BACKLIGHT_INCREASE(),
 };
